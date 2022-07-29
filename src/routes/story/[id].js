91dfd18ccb30e15c14c47d76d9e716d37a57c52f
Derek Mason <3derekmason@gmail.com>
2022-07-29 16:01:55 -0600

    story components/page init

---
+++ src/routes/story/[id].js
@@ -0,0 +1,19 @@
+// import db from '$lib/database';
+
+// /** @type {import('./__types/[id]').RequestHandler} */
+// export async function GET({ params }) {
+//   // `params.id` comes from [id].js
+//   const item = await db.get(params.id);
+
+//   if (item) {
+//     return {
+//       status: 200,
+//       headers: {},
+//       body: { item }
+//     };
+//   }
+
+//   return {
+//     status: 404
+//   };
+// }
---
+++ src/routes/story/[id].svelte
@@ -0,0 +1,8 @@
+<script>
+	// populated with data from the endpoint
+	export let story;
+</script>
+
+<div class="story">
+	{story}
+</div>
