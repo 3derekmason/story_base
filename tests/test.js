5f7c806d49a7af3b8f8f6eedc9f3d1226b19c6b0
Derek Mason <3derekmason@gmail.com>
2022-07-30 11:06:26 -0600

    home page selection test

--- tests/test.js
+++ tests/test.js
@@ -5,8 +5,15 @@ test('Start reading button loads text', async ({ page }) => {
 	expect(await page.innerText('button')).toBe('START READING!');
 });
 
-test('Start reading button redirects to home', async ({ page }) => {
-	await page.goto('/about');
-	await page.locator('text=START READING!').click();
-	expect(await page.innerText('h2')).not.toBe('Welcome');
+test('Buttons set correct selection', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.innerText('h4')).toBe('stories about everything...');
+	await page.locator('text=training').click();
+	expect(await page.innerText('h4')).toBe('stories about training.');
+	await page.locator('text=programming').click();
+	expect(await page.innerText('h4')).toBe('stories about programming.');
+	await page.locator('text=wellness').click();
+	expect(await page.innerText('h4')).toBe('stories about health.');
+	await page.locator('text=life').click();
+	expect(await page.innerText('h4')).toBe('stories about life.');
 });
