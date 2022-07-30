4ffd8921a7086bce18b41a6ede78b66e64ee5f6d
Derek Mason <3derekmason@gmail.com>
2022-07-30 10:16:35 -0600

    submit requirements

--- src/routes/submit/index.svelte
+++ src/routes/submit/index.svelte
@@ -1,3 +1,10 @@
 <section>
 	<h1>Submit your story!</h1>
+	<h2>Email submissions to <a href="mailto:thestorybase@gmail.com">thestorybase@gmail.com</a></h2>
+	<h3>Please include:</h3>
+	<ul>
+		<li>Your full name</li>
+		<li>A markdown or pdf file of your story</li>
+		<li>Any other contribution credits you'd like your story to include</li>
+	</ul>
 </section>
