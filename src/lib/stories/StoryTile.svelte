718d01bd2e49845f4280614dc7febbe43f6c3b1c
Derek Mason <3derekmason@gmail.com>
2022-08-01 08:37:13 -0600

    story tile styling

--- src/lib/stories/StoryTile.svelte
+++ src/lib/stories/StoryTile.svelte
@@ -1,6 +1,6 @@
 <script>
 	import ChartBubble from 'svelte-material-icons/ChartBubble.svelte';
-
+	import CircleSmall from 'svelte-material-icons/CircleSmall.svelte';
 	import CodeBraces from 'svelte-material-icons/CodeBraces.svelte';
 	import Dumbbell from 'svelte-material-icons/Dumbbell.svelte';
 	import HospitalBox from 'svelte-material-icons/HospitalBox.svelte';
@@ -10,38 +10,42 @@
 
 <div class="tile">
 	<div class="row">
-		<h2>{story.title}</h2>
-		<h4>by {story.author}</h4>
+		<h3>{story.author}</h3>
+		<CircleSmall />
+		<h6>{story.created_at}</h6>
+	</div>
+	<h2>{story.title}</h2>
+	<h4>{story.body}</h4>
+	<div class="row">
 		<ul>
 			{#each story.tags as tag}
 				{#if tag === 'training'}
-					<li><Dumbbell size="20" color="#ffffff" /></li>
+					<li><Dumbbell size="20" color="#444444" /></li>
 				{/if}
 				{#if tag === 'programming'}
-					<li><CodeBraces size="20" color="#ffffff" /></li>
+					<li><CodeBraces size="20" color="#444444" /></li>
 				{/if}
 				{#if tag === 'wellness'}
-					<li><HospitalBox size="20" color="#ffffff" /></li>
+					<li><HospitalBox size="20" color="#444444" /></li>
 				{/if}
 				{#if tag === 'life'}
-					<li><ChartBubble size="20" color="#ffffff" /></li>
+					<li><ChartBubble size="20" color="#444444" /></li>
 				{/if}
 			{/each}
 		</ul>
-	</div>
-
-	<h4>{story.body}</h4>
-	<div class="row">
+		<button>VIEW STORY</button>
 		<a href={story.link} target="_blank">Read on Medium</a>
-		<h6>{story.created_at}</h6>
 	</div>
 </div>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
 	.tile {
 		width: 90%;
 		height: 160px;
-		border: 1px solid var(--color-main-light);
+		border-bottom: 1px solid var(--color-main);
+		padding: 8px;
+		padding-bottom: 16px;
 	}
 	.row {
 		display: flex;
@@ -49,6 +53,7 @@
 		gap: 24px;
 	}
 	ul {
+		padding: 0;
 		display: flex;
 		gap: 16px;
 		list-style: none;
@@ -56,9 +61,28 @@
 	li {
 		width: 48px;
 		height: 24px;
-		border-radius: 8px;
-		background: var(--color-story-light);
+		border-radius: 12px;
+		border: 0.5px solid var(--color-main-light);
 		display: grid;
 		place-content: center;
+	}
+
+	h6,
+	h2,
+	h4 {
+		margin: 0;
+	}
+	h2 {
+		font-size: 20px;
+		font-weight: 600;
+	}
+
+	h4 {
+		margin-top: 16px;
+		font-family: 'Playfair Display', serif;
+	}
+
+	a {
+		text-transform: uppercase;
 	}
 </style>
