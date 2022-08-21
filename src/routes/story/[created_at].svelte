61f22b450e03995ed23e15e2361b797b64656f74
Derek Mason <3derekmason@gmail.com>
2022-08-21 09:09:39 -0600

    convert md to html and display story

--- src/routes/story/[created_at].svelte
+++ src/routes/story/[created_at].svelte
@@ -1,18 +1,20 @@
 <script>
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import showdown from 'showdown';
+	const converter = new showdown.Converter();
 	const created_at = $page.url.pathname.slice(7);
 	let displayStory;
 	onMount(() => {
 		fetch(`http://localhost:5000/api/file?added=${created_at}`)
 			.then((res) => res.json())
 			.then((data) => {
-				displayStory = data;
+				const html = converter.makeHtml(data);
+				displayStory = html;
 			});
 	});
-	const getStory = () => {};
 </script>
 
 <div class="story">
-	{JSON.stringify(displayStory)}
+	{@html displayStory}
 </div>
